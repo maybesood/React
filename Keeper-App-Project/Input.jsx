662b6bb90e1c339f37ca44885b00c45d4c29f64e
himansh <125412831+maybesood@users.noreply.github.com>
2023-07-12 17:54:19 +0530

    Create Input.jsx

---
+++ Keeper-App-Project/Input.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Input(props){
+  return(
+    <div>
+    <input type="text" placeholder="Username" />
+    <input type="password" placeholder="Password" />
+    <input type="password" placeholder="Confirm Password" />
+    </div>
+  );
+}
+
+export default Input;
